9cbef40386c65b0a2f4b3895611d9ee633d75b8d
serhatarslan-hub <sarslan@stanford.edu>
2019-04-09 17:45:55 -0700

    Web server functionality configurations

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -2,8 +2,8 @@
 
 source ./dctcp-ns2/settings.sh
 ./benchmark_sim.py --dctcp --timely --vegas
-#echo "Please navigate to:  http://<IP_ADDRESS> in your browser to view the results."
-#sudo python -m SimpleHTTPServer 80
+echo "Please navigate to:  http://<IP_ADDRESS> in your browser to view the results."
+sudo python -m SimpleHTTPServer 80
 
 
 
