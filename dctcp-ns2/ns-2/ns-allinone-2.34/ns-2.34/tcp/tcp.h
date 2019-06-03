e746898d4f9009f5d893746733e0dc139fa5cf92
Catalin Voss <catalin@cs.stanford.edu>
2019-06-03 00:53:20 -0700

    Fix patchedTimely_ type -- apparently needs to be int64

--- dctcp-ns2/ns-2/ns-allinone-2.34/ns-2.34/tcp/tcp.h
+++ dctcp-ns2/ns-2/ns-allinone-2.34/ns-2.34/tcp/tcp.h
@@ -608,7 +608,7 @@ protected:
 	double timely_decreaseFac_;	// Multiplicative decrease constant
 	int timely_HAI_thresh_;	// HAI mode threshold
 	double rttNoise_;  // RTT measurement noise (uniform in [0, rttNoiseMax_]); zero means no noise
-	bool patchedTimely_;
+	int patchedTimely_;
 
 	double timely_rate_;	// Current rate in bits/sec
 	double timely_prevRTT_;	// Previous RTT in sec
